c40d8128b311af3de96b5c48348e987cd304b1f3
sthancock <steven.hancock@ed.ac.uk>
2021-02-26 16:54:56 +0000

    Another debugging statement added.

--- photonCount.c
+++ photonCount.c
@@ -479,6 +479,9 @@ float **countPhotons(float *denoised,dataStruct *data,photonStruct *photonCount,
   thisGr=NULL;
 
   /*add noise photons*/
+  #ifdef DEBUG
+  fprintf(stdout,"Adding %d noise over %f signal %d\n",nNoise,photonCount->H,nPhotons);
+  #endif
   for(i=0;i<nNoise;i++){
     thisZ=(maxZ-minZ)*((float)rand()/(float)RAND_MAX)+minZ;
 
