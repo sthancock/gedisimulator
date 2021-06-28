eee86d00678ecba7439ceb9d7d52ab0db37f0af9
sthancock <steven.hancock@ed.ac.uk>
2021-06-28 21:09:05 +0100

    Small change to pulse normalisation.

--- gediIO.c
+++ gediIO.c
@@ -4150,7 +4150,7 @@ void applyPulseShape(gediIOstruct *gediIO,gediRatStruct *gediRat,waveStruct *wav
             tempGr[k][i]+=waves->ground[k][bin]*gediIO->pulse->y[j];
             tempC[k][i]+=waves->canopy[k][bin]*gediIO->pulse->y[j];
           }
-          contN+=gediIO->pulse->y[j]-minPulse;
+          contN+=1.0; //gediIO->pulse->y[j]-minPulse;
         }/*bin bound check*/
       }/*pulse bin loop*/
 
